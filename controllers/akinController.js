c784fd03cedb72451a875af7e0d2dfb18560f451
graduationProject0101 <83544640+graduationProject0101@users.noreply.github.com>
2021-07-15 21:47:21 +0200

    random items if new user

--- controllers/akinController.js
+++ controllers/akinController.js
@@ -12,20 +12,40 @@ exports.getRecommendations = catchAsync(async (req, res) => {
       req.body.samples
     );
 
-  if (!sampledRecommendations) {
-    return new AppError('No recommendations for this user yet', 404);
-  }
+  if (sampledRecommendations.length == 0) {
+    const Items = await Item.find();
+    const randomItems = getRandom(Items, req.body.samples);
+    res.status(200).json({
+      status: 'success',
+      items: randomItems,
+    });
+  } else {
+    for (let i = 0; i < sampledRecommendations.length; i++) {
+      const recommendedItems = await Item.findById(
+        sampledRecommendations[i].item
+      );
+      recommendedList.push(recommendedItems);
+    }
 
-  for (let i = 0; i < sampledRecommendations.length; i++) {
-    const recommendedItems = await Item.findById(
-      sampledRecommendations[i].item
-    );
-    recommendedList.push(recommendedItems);
+    res.status(200).json({
+      status: 'success',
+      length: sampledRecommendations.length,
+      recommendations: recommendedList,
+    });
   }
-
-  res.status(200).json({
-    status: 'success',
-    length: sampledRecommendations.length,
-    recommendations: recommendedList,
-  });
 });
+
+//to get random items if the user did not make any activities
+function getRandom(arr, n) {
+  var result = new Array(n),
+    len = arr.length,
+    taken = new Array(len);
+  if (n > len)
+    throw new RangeError('getRandom: more elements taken than available');
+  while (n--) {
+    var x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
